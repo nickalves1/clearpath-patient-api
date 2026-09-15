aa378d5240667603255ae1d0a8ea219f6aa28244
Nicolas Alves <nicolas.adspm@gmail.com>
2026-09-15 19:26:51 -0300

    Fix Amplify env vars not reaching the Next.js SSR runtime (#48)
    Amplify's app-level environment_variables are only guaranteed at
    build time, not automatically exposed to the Web Compute (SSR)
    runtime. Add a preBuild step that bakes them into .env.production so
    Next.js picks them up, and grant the app an iam_service_role_arn with
    SSM read + KMS decrypt so Amplify's own secrets injection stops
    failing silently during the build ("Setting Up SSM Secrets").
    
    Also add AUTH_URL, needed by Auth.js to build correct callback/signin
    URLs when the incoming Host header from CloudFront isn't reliably
    forwarded to the SSR function.

--- terraform/amplify.tf
+++ terraform/amplify.tf
@@ -18,11 +18,29 @@ data "aws_ssm_parameter" "auth_secret" {
   with_decryption = true
 }
 
+data "aws_iam_policy_document" "amplify_service_trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["amplify.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "amplify_service" {
+  name               = "clearpath-patient-app-amplify-service"
+  assume_role_policy = data.aws_iam_policy_document.amplify_service_trust.json
+}
+
 resource "aws_amplify_app" "patient_app" {
-  name         = "clearpath-patient-app"
-  repository   = "https://github.com/nickalves1/clearpath-patient-app"
-  access_token = data.aws_ssm_parameter.github_access_token.value
-  platform     = "WEB_COMPUTE"
+  name                 = "clearpath-patient-app"
+  repository           = "https://github.com/nickalves1/clearpath-patient-app"
+  access_token         = data.aws_ssm_parameter.github_access_token.value
+  platform             = "WEB_COMPUTE"
+  iam_service_role_arn = aws_iam_role.amplify_service.arn
 
   build_spec = <<-EOT
     version: 1
@@ -31,6 +49,7 @@ resource "aws_amplify_app" "patient_app" {
         preBuild:
           commands:
             - npm ci
+            - env | grep -e AUTH_URL -e AUTH_HYDRA_ID -e AUTH_HYDRA_SECRET -e AUTH_SECRET -e AUTH_HYDRA_ISSUER -e HYDRA_ADMIN_URL -e NEXT_PUBLIC_KRATOS_URL >> .env.production
         build:
           commands:
             - npm run build
@@ -49,6 +68,7 @@ resource "aws_amplify_app" "patient_app" {
   }
 
   environment_variables = {
+    AUTH_URL            = "https://app.clearpath.fitleads.com.br"
     AUTH_HYDRA_ID       = data.aws_ssm_parameter.auth_hydra_id.value
     AUTH_HYDRA_SECRET    = data.aws_ssm_parameter.auth_hydra_secret.value
     AUTH_SECRET          = data.aws_ssm_parameter.auth_secret.value
@@ -56,6 +76,37 @@ resource "aws_amplify_app" "patient_app" {
     HYDRA_ADMIN_URL      = "http://34.226.186.255:4445"
     NEXT_PUBLIC_KRATOS_URL = "https://kratos.clearpath.fitleads.com.br"
   }
+}
+
+data "aws_iam_policy_document" "amplify_service_permissions" {
+  statement {
+    sid    = "ReadAmplifyEnvSecrets"
+    effect = "Allow"
+    actions = [
+      "ssm:GetParametersByPath",
+      "ssm:GetParameters",
+      "ssm:GetParameter",
+      "ssm:DescribeParameters",
+    ]
+    resources = ["arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:parameter/amplify/${aws_amplify_app.patient_app.id}/*"]
+  }
+
+  statement {
+    sid       = "DecryptAmplifySsm"
+    effect    = "Allow"
+    actions   = ["kms:Decrypt"]
+    resources = [data.aws_kms_alias.ssm.target_key_arn]
+  }
+}
+
+resource "aws_iam_policy" "amplify_service_permissions" {
+  name   = "clearpath-patient-app-amplify-service-permissions"
+  policy = data.aws_iam_policy_document.amplify_service_permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "amplify_service_permissions" {
+  role       = aws_iam_role.amplify_service.name
+  policy_arn = aws_iam_policy.amplify_service_permissions.arn
 }
 
 resource "aws_amplify_branch" "main" {
